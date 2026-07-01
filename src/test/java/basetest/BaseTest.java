00b31c7eabc857b0e93e1a34c02ea3d3ca23809c
vignesh <balavignesht2810@gmail.com>
2026-07-01 09:26:20 +0530

    quit method

--- src/test/java/basetest/BaseTest.java
+++ src/test/java/basetest/BaseTest.java
@@ -2,6 +2,7 @@ package basetest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.time.Duration;
@@ -15,5 +16,9 @@ public class BaseTest {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+    }
+    @AfterClass
+    public void quitBrowser(){
+        driver.quit();
     }
 }
