e9bd0d1ba222e07b853a2447361ae3a10dc787a5
Eswaramoorthy <eswarmurugan143@gmail.com>
2026-07-01 09:19:09 +0530

    Added test case 16

--- src/main/java/org/zigWheelsAutomation/pages/LoginPage.java
+++ src/main/java/org/zigWheelsAutomation/pages/LoginPage.java
@@ -4,6 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static java.time.Duration.*;
 
 public class LoginPage {
     WebDriver driver;
@@ -15,13 +21,16 @@ public class LoginPage {
     WebElement login;
 
     @FindBy(xpath="//span[text()='Google']")
-    WebElement google;
+    public WebElement google;
 
     @FindBy(xpath = "//span[text()='Next']")
     WebElement next;
 
     @FindBy(id="identifierId")
     WebElement emailInput;
+
+    @FindBy(xpath = "//div[@id='i8']")
+    WebElement errorMsg;
 
 
     public void goLogin(){
@@ -38,6 +47,11 @@ public class LoginPage {
 
     public void clickNext(){
         next.click();
+    }
+
+    public String getErrorMessage(){
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        return wait.until(ExpectedConditions.visibilityOf(errorMsg)).getText();
     }
 
 }
