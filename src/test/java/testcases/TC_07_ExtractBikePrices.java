b34010752abeeea0c94d0793bfd13ff6509c6046
Saibal1009 <rmaballus8@gmail.com>
2026-07-05 22:45:19 +0530

    added logs for tc6,7,8

--- src/test/java/testcases/TC_07_ExtractBikePrices.java
+++ src/test/java/testcases/TC_07_ExtractBikePrices.java
@@ -1,6 +1,8 @@
 package testcases;
 
 import basetest.BaseTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import org.zigWheelsAutomation.pages.UpcomingBikesPage;
@@ -8,6 +10,7 @@ import java.util.List;
 
 public class TC_07_ExtractBikePrices extends BaseTest {
 
+    private static final Logger log = LoggerFactory.getLogger(TC_07_ExtractBikePrices.class);
     UpcomingBikesPage upcomingBikesPage;
     SoftAssert softAssert;
 
@@ -32,5 +35,6 @@ public class TC_07_ExtractBikePrices extends BaseTest {
             );
         }
         softAssert.assertAll();
+        log.info("Bike price filter is working");
     }
 }
